110075eb0deedb15940f72fc3702d4d4ae24b20f
jmessk <jmerusty@gmail.com>
2025-10-15 02:13:27 +0000

    Add benchmark cases

---
+++ benchmark/mid/mid.js
@@ -0,0 +1,10 @@
+export default async function fetch(input) {
+    let iter = 8000;
+    let state = 123456789;
+    
+    for (let i = 0; i < iter; i++) {
+        state = (state * 1664525 + 1013904223) & 0xffffffff;
+    }
+
+    return state;
+}
--- scripts/test-1.js
+++ scripts/test-1.js
@@ -2,11 +2,14 @@ export default async function fetch(input) {
 
     // const client = new HttpClient();
     // const response1 = await client.get("http://localhost/");
+    let iter = 20000;
+    let state = 123456789;
 
-    let count = 0;
-    for (let i = 0; i < 10000; i++) {
-        count += i;
+    for (let i = 0; i < iter; i++) {
+        state = (state * 1664525 + 1013904223) & 0xffffffff;
     }
+
+    return state;
 
     // const client = new HttpClient();
     // const response2 = await client.get("http://localhost/");
